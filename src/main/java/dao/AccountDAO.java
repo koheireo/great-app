a1df47fa3cd3b91af1c90355e49dec0298cb0a4f
koheireo <92348485+koheireo@users.noreply.github.com>
2021-10-14 10:18:04 +0900

    Update AccountDAO.java

--- src/main/java/dao/AccountDAO.java
+++ src/main/java/dao/AccountDAO.java
@@ -9,20 +9,20 @@ import java.sql.SQLException;
 import model.Account;
 
 public class AccountDAO {
-	String url = "jdbc:postgresql://localhost:5432/sukkiriShop";
-	String user = "postgres";
-	String password = "postgres";
+	String url = "jdbc:postgresql://ec2-52-87-123-108.compute-1.amazonaws.com:5432/dctris4sbf8pdc";
+	String user = "rmcyzvztzozyoj";
+	String password = "8db8839ec9559648cd2222dcc07db95bb796ec038904c7f1ef94fdf61daa28d9";
 
 	public Account findOne(String id, String pass) {
 		Account account = null;
 		try (Connection conn = DriverManager.getConnection(url, user, password)) {
-			PreparedStatement pStmt = conn.prepareStatement("SELECT * FROM \"ACCOUNT\" WHERE \"USER_ID\"=? AND \"PASS\"=?");
+			PreparedStatement pStmt = conn.prepareStatement("select * from account where id=? and pass=?");
 			pStmt.setString(1, id);
 			pStmt.setString(2, pass);
 			ResultSet rs = pStmt.executeQuery();
 			if (rs.next()) {
-				id = rs.getString("USER_ID");
-				pass = rs.getString("PASS");
+				id = rs.getString("id");
+				pass = rs.getString("pass");
 				account = new Account(id, pass);
 			}
 		} catch (SQLException e) {
